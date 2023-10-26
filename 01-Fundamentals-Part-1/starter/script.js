dd8b90dbc92087bec2497f2c0d78a3d1f7479beb
Tsula7 <giga.tsulaia@gmail.com>
2023-10-26 18:45:47 +0400

    j

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,7 @@
 let js ='amazing';
-if (js=== 'amazing') alert ('JavasCript is Fun!');
 
-40 + 8 + 23 - 10;
-console.log (40+8+23-10);
+console.log(40 + 8 + 23 - 10);
+console.log ("jonas");
+
+let firstname= "Giga"
+console.log(firstname)
