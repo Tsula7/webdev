9c0ee4ca4b5e6b24f82f08361eb4bc1e616a092e
Tsula7 <giga.tsulaia@gmail.com>
2023-10-30 18:04:17 +0400

    last

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,4 @@
+/*
 let js ='amazing';
 
 //console.log(40 + 8 + 23 - 10);
@@ -22,3 +23,27 @@ let population = "3 500 000"
 console.log(country)
 console.log(continent)
 console.log(population)
+
+
+true; 
+console.log(true)
+*/
+
+// Math Operators
+const now =2037;
+const ageJonas = now -1991
+const ageSarah =now - 2018;
+console.log(ageJonas,ageSarah);
+console.log(ageJonas*2, ageJonas /10, 2**3);
+//2**3 means 2 to power of 3 (ხარისხში აყვანა)
+
+
+
+
+//Assihnment Operators
+let x =10 + 5;
+x +=10; //x=x+10=25
+x *=4; //x=x*4
+x++; //x=x+1
+x--; //x=x-1
+console.log(x);
