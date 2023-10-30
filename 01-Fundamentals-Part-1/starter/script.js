286149099dc96b9c2bb2e41c4d18448048bd1d2c
Tsula7 <giga.tsulaia@gmail.com>
2023-10-30 18:04:21 +0400

    last

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,4 @@
-/*
+
 let js ='amazing';
 
 //console.log(40 + 8 + 23 - 10);
@@ -27,7 +27,7 @@ console.log(population)
 
 true; 
 console.log(true)
-*/
+
 
 // Math Operators
 const now =2037;
@@ -47,3 +47,23 @@ x *=4; //x=x*4
 x++; //x=x+1
 x--; //x=x-1
 console.log(x);
+
+// Comparison OPeratoprs
+console.log(ageJonas > ageSarah); //>, <, >=, <=
+console.log(ageSarah >=18);
+
+const isFullage = ageSarah >= 18;
+
+
+*/
+
+
+const now =2037;
+const ageJonas = now -1991
+const ageSarah =now - 2018;
+console.log(now-1991 > now - 2018);
+let x,y;
+x=y=25-10-5;//x=y=10, x=10
+console.log(x,y);
+const averageAge =(ageJonas+ageSarah)/2
+console.log(ageJonas,ageSarah, averageAge);
