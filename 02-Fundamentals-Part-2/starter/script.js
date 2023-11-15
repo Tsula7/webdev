3298690d037c6dbf00375a44329c8a40df1d7c7b
Tsula7 <giga.tsulaia@gmail.com>
2023-11-15 18:42:34 +0400

    g

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -28,11 +28,49 @@ const appleJuice = fruitProcessor(5,0);
 console.log(appleJuice)
 const appleOrangeJuice =fruitProcessor(6,7)
 console.log(appleOrangeJuice);
-*/
 
+
+//Function declaration
 function calcAge1(birthYeah){
     // const age = 2037 -birthYeah;
     return 2037- birthYeah;
 }
 
-calcAge1(1991);
+const age1 = calcAge1(1991);
+console.log(age1)
+
+//function expression
+const calcage2 =function(birthYeah){
+    return 2037 - birthYeah
+}
+const age2 = calcage2(1991);
+console.log (age1, age2);
+*/
+
+const calcAge3 = birthYeah => 2037 - birthYeah;
+const age3 = calcAge3(1991);
+console.log(age3)
+
+const yearsUntilRetirement = (birthYeah, firstname) => {
+    const age =2037-birthYeah;
+    const retirement = 65 - age;
+    return `${firstname} retires in ${retirement} years`;
+}
+
+console.log(yearsUntilRetirement(1991, 'Jonas'));
+console.log(yearsUntilRetirement(1988, 'Bob'));
+console.log(' ')
+function cutFruitPieces(fruit){
+    return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges){
+    const applePieces = cutFruitPieces(apples);
+    const orangePieces = cutFruitPieces(oranges);
+    const juice = `Juice with ${applePieces} pieces of apple and ${orangePieces} pieces of orange.`;
+    return juice;
+}
+
+console.log(fruitProcessor(2,3));
+
+
