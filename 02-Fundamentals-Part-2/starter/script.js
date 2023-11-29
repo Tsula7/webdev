f11e92de1684547ca45da3e7c7842c06cbde6642
Tsula7 <giga.tsulaia@gmail.com>
2023-11-29 19:53:35 +0400

    zs

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -265,7 +265,7 @@ console.log(jonas.age);
 console.log(jonas.age);
 console.log(jonas.getSummary ());
 
-*/
+
 
 const mark = {
     fullName: 'Mark Miller',
@@ -295,5 +295,148 @@ const bmiResult = john.calcBMI()>mark.calcBMI()? `${john.fullName}'s BMI (${john
 console.log(mark.calcBMI());
 console.log(john.calcBMI());
 console.log(bmiResult);
+
+
+console.log('lifting weights repetitiom 1')
+
+
+//for loop keeps running while conditio is TRUE
+for(let rep=1; rep <=10; rep++){
+    console.log(`lifting weights repetitiom ${rep}`)
+
+}
+
+
+const jonasArray = [
+    'Jonas',
+    'Schmedtman',
+    2037-1991,
+    'teacher',
+    ['michael', 'Peter', 'Steven'],
+    true
+];
+const types = [];
+
+
+
+
+for(let i = 0; i<jonasArray.length;i++){
+    console.log(jonasArray[i], typeof jonasArray[i] );
+   //ახაილ types მასივის შევსება for loop -ით
+    // types[i] = typeof jonasArray[i];
+    types.push (typeof jonasArray[i]);
+
+}
+console.log(types);
+
+const years=[1991,2007,1969,2020];
+
+const ages = [];
+for (let i=0; i<years.length; i++){
+  ages.push (2037- years[i]);
+
+}
+console.log(ages);
+
+// continue and break
+
+// continue ამ შემთხვევაში გამოტოვებს ყველა იმ მნიშვნოლობას რომელიც არ არის ტექსტური და შესაბამისად ლოგში ამოვა მარტო ტექსტური(string)მნიშვნელობები
+console.log('----მხოლოდ ტექსტური(string)----')
+for(let i = 0; i<jonasArray.length;i++){
+    if (typeof jonasArray[i] !== 'string' ) continue;
+    console.log(jonasArray[i], typeof jonasArray[i] );
+
+}
+console.log('----შეწყვიტე ციკლი პიველ რიცხვზე(number))----')
+
+for(let i = 0; i<jonasArray.length;i++){
+    if (typeof jonasArray[i] === 'number' ) break;
+    console.log(jonasArray[i], typeof jonasArray[i] );
+
+}
+
+// break
+
+
+//backward loop
+const jonas = [
+    'Jonas',
+    'Schmedtman',
+    2037-1991,
+    'teacher',
+    ['michael', 'Peter', 'Steven'],
+];
+for(let i=jonas.length-1;i>=0;i--){
+    console.log(i, jonas[i]);
+}
+for (let excercise =1; excercise<=4; excercise++){
+    console.log(`-------starting exercise${excercise} 🕰️`);
+for(let rep=1;rep<6;rep++){
+    console.log(`Exercise ${excercise}: Lifting weight repetition ${rep} 🏋️`);
+}
+
+
+}
+
+//while loop
+
+
+// for(let rep=1; rep <=10; rep++){
+//     console.log(`lifting weights repetitiom ${rep}`);
+
+// }
+
+let rep=1;
+while(rep<=10){
+    // console.log(`While lifting weights repetitiom ${rep}`);
+    rep++;
+
+}
+
+let dice = Math.trunc(Math.random()*6)+1;
+console.log(dice);
+
+while(dice !==6){
+    console.log(`You roller a ${dice}`);
+    dice = Math.trunc(Math.random()*6)+1;
+    if(dice===6) console.log('loop is to end..')
+
+}
+*/
+const calcTip = function (bill) {
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+  }
+  
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52,];
+
+
+const tips = [];
+const totals =[];
+
+for (let i=0; i<bills.length ; i++){
+ tips.push(calcTip(bills[i]));
+};
+
+for (let i=0; i<bills.length ; i++){
+    totals.push(calcTip(bills[i])+bills[i]);
+   }
+
+
+console.log(bills);   
+console.log(tips);
+console.log(totals);
+
+
+const calcAverage = function (arr) {
+    let sum=0
+
+for (let i=0; i<arr.length;i++){
+    sum = sum+arr[i]
+}
+    return sum/arr.length
+
+
+}
+console.log(calcAverage(totals));
 
 
